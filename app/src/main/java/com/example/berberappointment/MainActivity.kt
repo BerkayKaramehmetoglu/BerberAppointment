1a2c5a16d478c8d93fe1dc59d912805bc72c181f
Berkay Karamehmetoglu <berkaykaramehmetoglu54@gmail.com>
2023-12-18 17:26:45 +0300

    Created Project
    Created the project and connect to Firebase

--- app/src/main/java/com/example/berberappointment/MainActivity.kt
+++ app/src/main/java/com/example/berberappointment/MainActivity.kt
@@ -2,18 +2,13 @@ package com.example.berberappointment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.Firebase
-import com.google.firebase.database.database
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val database = Firebase.database
-        val myRef = database.getReference("message")
-
-        myRef.setValue("Hello, World!")
 
     }
 }
